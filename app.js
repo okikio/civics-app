2516298876d8cb630e98ab8a269454c94407bf34
whitelightG <femikiki@gmail.com>
2019-01-15 04:59:15 +0000

    Update

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-// app.set('view cache', true);
+app.set('view cache', true);
 
 app.use(logger('dev'));
 app.use(express.json());
